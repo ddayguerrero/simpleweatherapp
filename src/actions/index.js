eb1bf1a25a8b5f40bfc02d42b6d9be070b4a8279
Darrel Guerrero <ddayy.guerrero@hotmail.com>
2017-05-14 16:40:55 -0400

    Add open-weather-api GET call

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+
+const API_KEY = '177735aa2f5d8377b853b191814be23a';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city){
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+  
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
